57ad4ac4aeec296ade98546f5e11e63f088f4fd6
austsxk <austsxk@163.com>
2025-02-13 18:31:49 +0800

    update my data

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,22 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Alex Running Life',
+  siteUrl: 'https://alexs.austsxk.com',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'Alex Running Life',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'AI',
+      url: 'https://next.sunls.de/',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'AI算卦',
+      url: 'https://divination.sunls.de',
+    },
+    {
+      name: '博客',
+      url: 'https://austsxk.com',
     },
   ],
 };
